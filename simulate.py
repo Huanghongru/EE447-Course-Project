91e3d0274e5ef40e9e8c1af418ebf126a5864621
huanghongru <huanghongru1997@gmail.com>
2018-05-22 07:33:07 -0700

    update simulation functions

--- simulate.py
+++ simulate.py
@@ -19,22 +19,27 @@ def simulation(graph, q_ratio, hd=True, pagerank=True,
     if hd:
         hd_seed = HD(graph.copy(), q_ratio)
         _, hd_cr = cascade(graph, hd_seed)
+        print "cascade the graph with HD algorithm successfully!!"
         result['HD_cr'] = hd_cr
     if pagerank:
         pr_seed = PageRank(graph.copy(), q_ratio)
         _, pr_cr = cascade(graph, pr_seed)
+        print "cascade the graph with PageRank algorithm successfully!!"
         result['PR_cr'] = pr_cr
     if ci:
         ci_seed = CI(graph.copy(), q_ratio)
         _, ci_cr = cascade(graph, ci_seed)
+        print "cascade the graph with CI algorithm successfully!!"
         result['CI_cr'] = ci_cr
     if kcore:
         kc_seed = K_core(graph.copy(), q_ratio)
         _, kc_cr = cascade(graph, kc_seed)
+        print "cascade the graph with k-core algorithm successfully!!"
         result['KC_cr'] = kc_cr
     if fanshen:
         fs_seed = fanshen(graph.copy(), q_ratio)
         _, fs_cr = cascade(graph, fs_seed)
+        print "cascade the graph with fanshen algorithm successfully!!"
         result['FS_cr'] = fs_cr
     return result
 
@@ -44,8 +49,10 @@ def cascRate_vs_qRatio(graph, q_ratio_group):
     pass
 
 def main():
-    G = gen_random_graph(1000, 0.0015, uniform_max=0.5)
-    print simulation(G, 0.005, fanshen=False)
+    # G = gen_random_graph(1000, 0.0015, uniform_max=0.5)
+    # print simulation(G, 0.005, fanshen=False)
+    G = load_graph('brightkite.txt')
+    print simulation(G, 0.005, fanshen=False, ci=False)
 
 if __name__ == '__main__':
     main()
